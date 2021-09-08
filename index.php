baa0e4dc2ee3c204700df29e8d365d3cee542cbe
Prithesh <prithesh07@gmail.com>
2021-09-08 06:32:31 +0000

    updated

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <html> 
 	<head> 
-	<title>PHP mayank Test</title>
+	<title>PHP Prithesh Test</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
 	<h1>PHP 2021 Test</h1>
-		<p><b>  Mayank modi Version9</b></p>
+		<p><b>  Prithesh S Version1</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information VERSION2 </h2> 
+	<h2>PHP Information VERSION1 </h2> 
 		<p> <?php phpinfo(); ?> </p> 
 	</body> 
 </html>
